26a011a5348c78a2363c6fb619143df8b9c94ff1
Diego Gomes <diegoramosgomes5@gmail.com>
2022-11-27 12:22:36 -0300

    chore - adicionado ignorar logs causados pelo markdown do tópico

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { Routes } from "./src/Routes";
+import { LogBox } from "react-native";
 
 export default function App() {
+  LogBox.ignoreLogs([
+    'fontFamily "Courier" is not a system font and has not been loaded through Font.loadAsync.',
+    'Input data should be a String'
+  ])
+
   return (
     <NavigationContainer>
       <Routes />
