447dab78680940f7251a1a2632ca1eea3848b092
Diego Gomes <diegoramosgomes5@gmail.com>
2022-12-05 14:52:26 -0300

    feat - busca de conteudo no perfil

--- src/Pages/ProfilePage/index.tsx
+++ src/Pages/ProfilePage/index.tsx
@@ -3,22 +3,29 @@ import { AccountStackRoutes } from "../../Routes/AuccountRoutes";
 import { useCallback, useContext, useEffect, useState } from "react";
 import AuthContext from "../../Contexts/AuthContext";
 import { useProfile } from "../../Hooks/useProfile";
-import { ActivityIndicator, FlatList, RefreshControl, View } from "react-native";
+import { ActivityIndicator, FlatList, RefreshControl, TextInput, View } from "react-native";
 import { HomeListItem } from "../../Components/HomeListItem";
+import { MagnifyingGlass } from "phosphor-react-native";
 
 
-export const ProfilePage = ({ navigation, route }: NativeStackNavigationProp<AccountStackRoutes, 'ProfilePage'>) => {
+export const ProfilePage = ({ route }: NativeStackNavigationProp<AccountStackRoutes, 'ProfilePage'>) => {
   const { user } = useContext(AuthContext)
   const { getUserContents } = useProfile()
 
   const [page, setPage] = useState(1)
   const [contents, setContents] = useState([])
   const [isLoading, setIsLoading] = useState(false)
+  const [searchText, setSearchText] = useState('')
   const [refreshing, setRefreshing] = useState(false)
   const [hasMoreData, setHasMoreData] = useState(true)
 
   const username = route.params?.username ?? user.username
   const perPage = 5
+
+  const filteredContents = contents?.filter((item) => {
+    return item?.body?.toLowerCase().includes(searchText.toLowerCase()) ||
+      item?.title?.toLowerCase().includes(searchText.toLowerCase())
+  })
 
   useEffect(() => {
     loadContents().then()
@@ -51,6 +58,33 @@ export const ProfilePage = ({ navigation, route }: NativeStackNavigationProp<Acc
 
   return (
     <>
+      <View style={{
+        flexDirection: 'row',
+        borderWidth: 1,
+        borderRadius: 5,
+        borderColor: '#c4c4c4',
+        marginTop: 8,
+        marginHorizontal: 16
+      }}>
+        <View style={{
+          backgroundColor: '#c4c4c4',
+          paddingHorizontal: 8,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }} >
+          <MagnifyingGlass color={'black'} weight={'bold'} size={18}/>
+        </View>
+        <TextInput
+          style={{
+            flex: 1,
+            paddingHorizontal: 8,
+            height: 35,
+          }}
+          placeholder={"Digite um termo para busca"}
+          value={searchText}
+          onChangeText={setSearchText}
+        />
+      </View>
       <FlatList
         refreshControl={
           <RefreshControl
@@ -59,9 +93,10 @@ export const ProfilePage = ({ navigation, route }: NativeStackNavigationProp<Acc
           />
         }
         contentContainerStyle={{
-          margin: 16
+          margin: 16,
+          paddingBottom: 24
         }}
-        data={contents}
+        data={searchText != '' ? filteredContents : contents}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => <HomeListItem post={item}/>}
         ItemSeparatorComponent={() => <View style={{ height: 16 }}/>}
